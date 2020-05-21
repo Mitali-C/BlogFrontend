6b3afa371d99e2875d852e8bfbb4e62aff576ff7
Mitali Chaudhari <mitalichaudhari098@gmail.com>
2020-05-21 12:51:57 +0530

    redirects to test deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Landing from './components/landing/Landing';
 import About from './components/about/About';
 
