fb2127cbe60fb7a1eeac7efbb5b56dc4eb6d187d
Mitali Chaudhari <mitalichaudhari098@gmail.com>
2020-05-21 13:29:30 +0530

    history.push added

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 
 class Landing extends React.Component{
   render(){
     return(
-      <h1 onClick={()=>{window.location.href='./about'}}>LANDING PAGE</h1>
+      <h1 onClick={()=>{this.props.history.push('./about')}}>LANDING PAGE</h1>
     )
   }
 }
 
-export default Landing;
+export default withRouter(Landing);
